a14e36ac0a814533cf19b99ae8781a465c6011a0
Nadine Seibel <nadine.seibel@bi-so.de>
2023-04-12 17:13:09 +0200

    sorted out typescript + rollup
    generated js code is now slightly more modern (still var instead of let though)

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  env: { node: true },
-  parserOptions: {
-    project: './tsconfig.lint.json',
-    tsconfigRootDir: __dirname,
-  },
-};
-
-module.exports = config;
--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ const options = {
   output: {
     dir: './www/',
     format: 'cjs',
-    sourcemap: 'inline',
+    sourcemap: 'inline'
   },
   external: ['cordova'],
   plugins: [
